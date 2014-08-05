cc97864e5763eedfedc1e99f549d55a2a455c4d3
dianedouglas <diane.douglas1@gmail.com>
2014-08-05 00:21:09 -0700

    Add second test for power operator and fixed wording on power operator's first test.

--- spec/word_calculator_spec.rb
+++ spec/word_calculator_spec.rb
@@ -19,7 +19,11 @@ describe('calculate') do
     calculate('What is 5 divided by 2?').should(eq(2.5))
   end
 
-  it('subtracts two integers given in a sentence.') do
+  it('raises the first integer to the second integer given in a sentence.') do
     calculate('What is 2 to the 3rd power?').should(eq(8))
+  end
+
+  it('raises the first integer to the second integer when worded differently in a sentence.') do
+    calculate('What is 2 to the power of 3?').should(eq(8))
   end
 end
