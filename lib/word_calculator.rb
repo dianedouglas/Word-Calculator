e2fd0320e33287cc81a53b159a746733feaf3fbc
dianedouglas <diane.douglas1@gmail.com>
2014-08-04 23:44:58 -0700

    Add a condition to store the word for the operator and use it to choose between adding and subtracting the two input integers, outputting either the sum or the difference.

--- lib/word_calculator.rb
+++ lib/word_calculator.rb
@@ -1,11 +1,29 @@
 def calculate(sentence)
   operands = []
+  operators = []
+  prev_is_operand = false
 
   sentence.split(" ").each do |word|
-    if ((/[0-9]/ =~ word.split("")[0]) == 0)
-      operands << word.to_i
+    if (prev_is_operand)
+      operators << word
+      prev_is_operand = false
     end
+
+    if ((/[0-9]/ =~ word.split("")[0]) == 0)
+      #if first letter contains a number
+      operands << word.to_i
+      prev_is_operand = true
+    end
+
   end
-  answer = operands[0] + operands[1]
+
+  if operators[0] == 'plus'
+    answer = operands[0] + operands[1]
+  elsif(operators[0] == 'minus')
+    answer = operands[0] - operands[1]
+  end
   
+  answer
 end
+
+# puts calculate('What is 5 minus 3?')
--- spec/word_calculator_spec.rb
+++ spec/word_calculator_spec.rb
@@ -2,8 +2,12 @@ require 'rspec'
 require 'word_calculator'
 
 describe('calculate') do
-  
+
   it('adds two integers given in a sentence.') do
     calculate('What is 5 plus 3?').should(eq(8))
+  end
+
+  it('subtracts two integers given in a sentence.') do
+    calculate('What is 5 minus 3?').should(eq(2))
   end
 end
